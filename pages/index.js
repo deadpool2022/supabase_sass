9274ef5178544f697421dadaf26b1cdc78820a2b
deadpool2022 <vikasgl2021@outlook.com>
2021-12-03 23:17:10 +0530

    showed data in supabase in browser

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
-import Head from 'next/head'
+import { supabase } from "../utils/supabase.js";
+import Link from "next/link";
 
-export default function Home() {
+export default function Home({ lessons }) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      Working
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      {lessons.map((lesson) => (
+        <Link key={lesson.id} href={`/${lesson.id}`}>
+          <a className='p-8 h-40 mb-4 rounded shadow text-xl flex'>
+            {lesson.title}
+          </a>
+        </Link>
+      ))}
     </div>
-  )
+  );
 }
+
+export const getStaticProps = async () => {
+  const { data: lessons } = await supabase.from("lesson").select("*");
+  return {
+    props: {
+      lessons,
+    },
+  };
+};
